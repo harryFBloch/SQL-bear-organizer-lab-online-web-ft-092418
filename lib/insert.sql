0225cce23f1c820350b3c7cfcb2e3d6dc9e5126b
Harry Bloch <harryfbloch@gmail.com>
2018-10-22 18:57:45 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,4 +2,5 @@ INSERT INTO bears(name, age, color) VALUES ("Mr. Chocolate", 3, "brown");
 INSERT INTO bears(name, alive, id) VALUES ("Rowdy", true, 201);
 INSERT INTO bears(name, gender, age) VALUES ("Tabitha", "male", 21);
 INSERT INTO bears (name, alive, age) VALUES 
-("Sergeant Brown", true, 15)
+("Sergeant Brown", true, 15);
+INSERT INTO BEARS (name, alive, age) VALUES("Melissa", true, 12);
